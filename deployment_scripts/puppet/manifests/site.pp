898b3351ca43609afcb5c1cf7afeb07b2797a1d0
huntxu <mhuntxu@gmail.com>
2015-04-17 17:41:10 +0800

    Use pacemaker to manage the service in HA deployment mode
    Signed-off-by: huntxu <mhuntxu@gmail.com>

--- deployment_scripts/puppet/manifests/site.pp
+++ deployment_scripts/puppet/manifests/site.pp
@@ -1,5 +1,14 @@
 $fuel_settings = parseyaml($astute_settings_yaml)
 
 if $fuel_settings {
-  class { 'lbaas': }
+  class { 'lbaas':
+    ha_mode => $fuel_settings['deployment_mode'] ? {
+      'ha_compact' => true,
+      default      => false,
+    },
+    primary_controller => $fuel_settings['role'] ? {
+      'primary-controller' => true,
+      default              => false
+    },
+  }
 }
--- deployment_scripts/puppet/modules/lbaas/manifests/init.pp
+++ deployment_scripts/puppet/modules/lbaas/manifests/init.pp
@@ -3,6 +3,8 @@ class lbaas (
   $lbaas_ovs_use_veth     = true,
   $lbaas_device_driver    = 'neutron.services.loadbalancer.drivers.haproxy.namespace_driver.HaproxyNSDriver',
   $lbaas_plugin           = 'neutron.services.loadbalancer.plugin.LoadBalancerPlugin',
+  $ha_mode                = false,
+  $primary_controller     = false,
 ) {
 
   include lbaas::params
@@ -11,9 +13,91 @@ class lbaas (
     ensure => present,
   }
 
+  if $ha_mode {
+
+    $service_ensure = 'stopped'
+    $enabled        = false
+
+    file { 'ocf-eayun-path':
+      ensure  => directory,
+      path    =>'/usr/lib/ocf/resource.d/eayun',
+      recurse => true,
+      owner   => 'root',
+      group   => 'root',
+    }
+
+    file { 'lbaas-agent-ocf-file':
+      path    => '/usr/lib/ocf/resource.d/eayun/neutron-agent-lbaas',
+      mode    => '0755',
+      owner   => 'root',
+      group   => 'root',
+      source  => "puppet:///modules/lbaas/ocf/neutron-agent-lbaas",
+      require => File['ocf-eayun-path'],
+    }
+
+#fuel-plugins system doesn't have 'primary-controller' role so
+#we have to separate controllers' deployment here using waiting cycles.
+
+    if $primary_controller {
+
+      cs_resource { "p_${lbaas::params::lbaas_agent_service}":
+        ensure          => present,
+        primitive_class => 'ocf',
+        provided_by     => 'eayun',
+        primitive_type  => 'neutron-agent-lbaas',
+        complex_type    => 'clone',
+        ms_metadata     => { 'interleave' => 'true' },
+        operations      => {
+          'monitor' => {
+            'interval' => '20',
+            'timeout'  => '10',
+          },
+          'start'   => {
+            'timeout' => '80',
+          },
+          'stop'    => {
+            'timeout' => '80',
+          }
+        }
+      }
+
+      File['lbaas-agent-ocf-file'] ->
+        Cs_resource["p_${lbaas::params::lbaas_agent_service}"] ->
+          Service[$lbaas::params::lbaas_agent_service]
+
+    } else {
+
+      exec {'waiting-for-lbaas-agent':
+        tries     => 10,
+        try_sleep => 20,
+        command   => "pcs resource show p_neutron-lbaas-agent > /dev/null 2>&1",
+        path      => '/usr/sbin:/usr/bin',
+      }
+
+      File['lbaas-agent-ocf-file'] ->
+        Exec['waiting-for-lbaas-agent'] ->
+          Service[$lbaas::params::lbaas_agent_service]
+
+    }
+
+    Service<| title == "${lbaas::params::lbaas_agent_service}" |> {
+      enable     => true,
+      ensure     => 'running',
+      hasstatus  => true,
+      hasrestart => false,
+      provider   => 'pacemaker',
+    }
+
+  } else {
+
+    $service_ensure = 'running'
+    $enabled        = true
+
+  }
+
   service { $lbaas::params::lbaas_agent_service:
-    ensure => running,
-    enable => true,
+    ensure => $service_ensure,
+    enable => $enabled,
   }
 
   neutron_lbaas_config {
