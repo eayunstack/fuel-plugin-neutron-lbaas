89ab74ffe274d24e2a6dd84ed6521f3951526a64
liuzhuangzhuang <vpbvmw651078@gmail.com>
2017-09-12 10:36:55 +0800

    solve plug-in deployment failure caused by hyper-c
    Feature-implementation: http://192.168.15.2/issues/10931
    Signed-off-by: liuzhuangzhuang <vpbvmw651078@gmail.com>

--- deployment_scripts/puppet/manifests/site.pp
+++ deployment_scripts/puppet/manifests/site.pp
@@ -1,15 +1,18 @@
 $fuel_settings = parseyaml($astute_settings_yaml)
 
 if $fuel_settings {
+  $roles = node_roles($fuel_settings['nodes'], $fuel_settings['uid'])
+  if 'primary-controller' in $roles {
+    $is_primary_controller = true
+  } else {
+    $is_primary_controller = false
+  }
   class { 'lbaas':
     ha_mode => $fuel_settings['deployment_mode'] ? {
       'ha_compact' => true,
       default      => false,
     },
-    primary_controller => $fuel_settings['role'] ? {
-      'primary-controller' => true,
-      default              => false
-    },
+    primary_controller => $is_primary_controller,
     auth_vip => $fuel_settings['management_vip'],
   }
 }
